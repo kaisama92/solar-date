86346e7ac10ff45797f694fa82c06427616b3773
Kai Clausen <kaiclausen123@gmail.com>
2023-02-03 14:45:21 -0800

    create constructor for YearsUntil and corresponding test.

---
+++ __tests__/years-until.test.js
@@ -0,0 +1,19 @@
+import EarthAge from '../src/js/earthDate';
+import YearsUntil from '../src/js/years-until';
+
+describe('YearsUntil', () => {
+  let myAge;
+  let until35;
+
+  beforeEach(() =>{
+    myAge = new EarthAge("kai", 25);
+  });
+  beforeEach(() => {
+    until35 = new YearsUntil(35, myAge)
+  });
+
+  test('should create an object with the key this.difference that determines how many years until the given age', () => {
+    expect(until35.differnce).toEqual(10);
+  });
+    
+})
---
+++ src/js/years-until.js
@@ -0,0 +1,6 @@
+export default class YearsUntil {
+
+  constructor(input, object){
+    
+  }
+}
