47dd6797455a3d4191dd9b6aea45e2cfb93a287e
Kai Clausen <kaiclausen123@gmail.com>
2023-02-03 14:28:49 -0800

    refactor earthDate.js and corresponding test file. Delete mercury-date and corresponding test file.

--- __tests__/earthDate.test.js
+++ __tests__/earthDate.test.js
@@ -5,29 +5,8 @@ describe('EarthAge', () => {
   test('should create an object with properties this.name, this.age, this.mercuryAge, this.venusAge, this.marsAge, and this.jupiterAge', () => {
     const myEarthAge = new EarthAge("kai", 25);
     expect(myEarthAge).toEqual({"name": "kai", "age": 25, "mercuryAge": 104.17, "venusAge": 40.32, "marsAge": 13.30, "jupiterAge": 2.11})
-  })
+  });
 
-  // test('Should find the mercury age of a person born on earth', () => {
-  //   const myEarthAge = new EarthAge("kai", 25);
-  //   myEarthAge.findMercury();
-  //   expect(myEarthAge.mercury).toEqual(104.17);
-  // }); 
+  
 
-  // test('Should find the venus age of a person born on earth', () => {
-  //   const myEarthAge = new EarthAge("kai", 25);
-  //   myEarthAge.findVenus();
-  //   expect(myEarthAge.venus).toEqual(40.32);
-  // });
-
-  // test('should find the mars age of a person born on earth', () => {
-  //   const myEarthAge = new EarthAge("kai", 25);
-  //   myEarthAge.findMars();
-  //   expect(myEarthAge.mars).toEqual(13.30)
-  // });
-
-  // test('should find the jupiter age of a person born on earth', () => {
-  //   const myEarthAge = new EarthAge("kai", 25);
-  //   myEarthAge.findJupiter();
-  //   expect(myEarthAge.jupiter).toEqual(2.11);
-  // });
 })
--- src/js/earthDate.js
+++ src/js/earthDate.js
@@ -9,23 +9,6 @@ export default class EarthAge {
     this.jupiterAge = parseFloat((age / 11.86).toFixed(2)); 
   }
 
-  // findMercury() {
-  //   let mercury = this.age / 0.24;
-  //   this.mercury = parseFloat(mercury.toFixed(2));
-  // }
 
-  // findVenus() {
-  //   let venus = this.age / 0.62;
-  //   this.venus = parseFloat(venus.toFixed(2));
-  // }
-
-  // findMars() {
-  //   let mars = this.age / 1.88;
-  //   this.mars = parseFloat(mars.toFixed(2));
-  // }
-
-  // findJupiter() {
-  //   let jupiter = this.age / 11.86;
-  //   this.jupiter = parseFloat(jupiter.toFixed(2));
-  // }
+  
 }
