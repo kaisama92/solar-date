ee55b2c3c79c8935d82903d7a489f2f13ecce8c1
Kai Clausen <kaiclausen123@gmail.com>
2023-02-03 13:54:15 -0800

    onMercury() passes the corresponding test.

--- __tests__/years-since.test.js
+++ __tests__/years-since.test.js
@@ -7,7 +7,13 @@ describe('YearsSince', () => {
     const myAge = new EarthAge("kai", 25);
     const sinceFive = new YearsSince(5, myAge);
     expect(sinceFive.difference).toEqual(20);
-  })
+  });
+
+  test('should determine what the difference equates to in Mercury years', () => {
+    const myAge = new EarthAge("kai", 25);
+    const sinceFive = new YearsSince(5, myAge);
+    expect(sinceFive.onMercury()).toEqual(83.33);
+  });
 
   // test('should determine how many years have occured since two given times', () => {
   //   const earthAge = new EarthAge("kai", 25);
--- src/js/years-since.js
+++ src/js/years-since.js
@@ -3,4 +3,8 @@ export default class YearsSince{
   constructor(input, object) {
     this.difference = object.age - input;
   }
+
+  onMercury(){
+    return parseFloat((this.difference / 0.24).toFixed(2));
+  }
 }
