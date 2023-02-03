d7c38e18c9b9b224f2da346f1b2773fef82a8bf0
Kai Clausen <kaiclausen123@gmail.com>
2023-02-03 11:31:27 -0800

    WIP - refactoring code.

--- __tests__/earthDate.test.js
+++ __tests__/earthDate.test.js
@@ -7,27 +7,32 @@ describe('EarthAge', () => {
     expect(myEarthAge).toEqual({"name": "Kai", "age": 25});
   });
 
-  test('Should find the mercury age of a person born on earth', () => {
+  test('should create an object with properties this.name, this.age, and this.mercuryAge', () => {
     const myEarthAge = new EarthAge("kai", 25);
-    myEarthAge.findMercury();
-    expect(myEarthAge.mercury).toEqual(104.17);
-  }); 
+    expect(myEarthAge).toEqual({"name": "kai", "age": 25, "mercuryAge": 104.17})
+  })
 
-  test('Should find the venus age of a person born on earth', () => {
-    const myEarthAge = new EarthAge("kai", 25);
-    myEarthAge.findVenus();
-    expect(myEarthAge.venus).toEqual(40.32);
-  });
+  // test('Should find the mercury age of a person born on earth', () => {
+  //   const myEarthAge = new EarthAge("kai", 25);
+  //   myEarthAge.findMercury();
+  //   expect(myEarthAge.mercury).toEqual(104.17);
+  // }); 
 
-  test('should find the mars age of a person born on earth', () => {
-    const myEarthAge = new EarthAge("kai", 25);
-    myEarthAge.findMars();
-    expect(myEarthAge.mars).toEqual(13.30)
-  });
+  // test('Should find the venus age of a person born on earth', () => {
+  //   const myEarthAge = new EarthAge("kai", 25);
+  //   myEarthAge.findVenus();
+  //   expect(myEarthAge.venus).toEqual(40.32);
+  // });
 
-  test('should find the jupiter age of a person born on earth', () => {
-    const myEarthAge = new EarthAge("kai", 25);
-    myEarthAge.findJupiter();
-    expect(myEarthAge.jupiter).toEqual(2.11);
-  });
+  // test('should find the mars age of a person born on earth', () => {
+  //   const myEarthAge = new EarthAge("kai", 25);
+  //   myEarthAge.findMars();
+  //   expect(myEarthAge.mars).toEqual(13.30)
+  // });
+
+  // test('should find the jupiter age of a person born on earth', () => {
+  //   const myEarthAge = new EarthAge("kai", 25);
+  //   myEarthAge.findJupiter();
+  //   expect(myEarthAge.jupiter).toEqual(2.11);
+  // });
 })
--- src/js/earthDate.js
+++ src/js/earthDate.js
@@ -5,23 +5,23 @@ export default class EarthAge {
     this.age = age;
   }
 
-  findMercury() {
-    let mercury = this.age / 0.24;
-    this.mercury = parseFloat(mercury.toFixed(2));
-  }
+  // findMercury() {
+  //   let mercury = this.age / 0.24;
+  //   this.mercury = parseFloat(mercury.toFixed(2));
+  // }
 
-  findVenus() {
-    let venus = this.age / 0.62;
-    this.venus = parseFloat(venus.toFixed(2));
-  }
+  // findVenus() {
+  //   let venus = this.age / 0.62;
+  //   this.venus = parseFloat(venus.toFixed(2));
+  // }
 
-  findMars() {
-    let mars = this.age / 1.88;
-    this.mars = parseFloat(mars.toFixed(2));
-  }
+  // findMars() {
+  //   let mars = this.age / 1.88;
+  //   this.mars = parseFloat(mars.toFixed(2));
+  // }
 
-  findJupiter() {
-    let jupiter = this.age / 11.86;
-    this.jupiter = parseFloat(jupiter.toFixed(2));
-  }
+  // findJupiter() {
+  //   let jupiter = this.age / 11.86;
+  //   this.jupiter = parseFloat(jupiter.toFixed(2));
+  // }
 }
