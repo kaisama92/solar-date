bba8f1e96ce250e1daa17b4ab926b59d0f829b2c
Kai Clausen <kaiclausen123@gmail.com>
2023-02-03 16:09:35 -0800

    refactor years-since.test.js

--- __tests__/years-since.test.js
+++ __tests__/years-since.test.js
@@ -2,38 +2,27 @@ import YearsSince from "../src/js/years-since";
 import EarthAge from "../src/js/earthDate";
 
 describe('YearsSince', () => {
+  const myAge = new EarthAge("kai", 25);
+  const sinceTwelve = new YearsSince(12, myAge);
+
 
   test('should create an object that contains the key this.difference', () => {
-    const myAge = new EarthAge("kai", 25);
-    const sinceFive = new YearsSince(5, myAge);
-    expect(sinceFive.difference).toEqual(20);
+    expect(sinceTwelve.difference).toEqual(20);
   });
 
   test('should determine what the difference equates to in Mercury years', () => {
-    const myAge = new EarthAge("kai", 25);
-    const sinceFive = new YearsSince(5, myAge);
-    expect(sinceFive.onMercury()).toEqual(83.33);
+    expect(sinceTwelve.onMercury()).toEqual(54.17);
   });
 
   test('should determine what this.difference equates to in Venus years', () => {
-    const myAge = new EarthAge("kai", 25);
-    const sinceTwelve = new YearsSince(12, myAge);
     expect(sinceTwelve.onVenus()).toEqual(20.97);
   });
 
   test('should determine what this.difference equates to in Mars years', () => {
-    const myAge = new EarthAge("kai", 25);
-    const sinceTwelve = new YearsSince(12, myAge);
     expect(sinceTwelve.onMars()).toEqual(6.91)
   });
 
   test('should determine what this.difference equates to in Mars years', () => {
-    const myAge = new EarthAge("kai", 25);
-    const sinceTwelve = new YearsSince(12, myAge);
     expect(sinceTwelve.onJupiter()).toEqual(1.10);
   });
-  // test('should determine how many years have occured since two given times', () => {
-  //   const earthAge = new EarthAge("kai", 25);
-  //   expect(yearsSince(5, earthAge)).toEqual(20);
-  // });
 })
