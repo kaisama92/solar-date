cf3fab0faa465dba3a3ed46e45e064c12ed3b0af
Kai Clausen <kaiclausen123@gmail.com>
2023-02-03 16:10:25 -0800

    made sure eslint was working.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-vars */
 import YearsUntil from './js/years-since';
 import YearsSince from './js/years-since';
 import EarthDate from './js/earthDate';
+/* eslint-enable no-unused-vars */
 
 //UI Logic
