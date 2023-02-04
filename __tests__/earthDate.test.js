53170467069d2c74abb07bb69cbedec7bc51ac69
Kai Clausen <kaiclausen123@gmail.com>
2023-02-03 16:49:27 -0800

    update earthDate.js and earthDate.test.js to keep UI Logic DRY

--- __tests__/earthDate.test.js
+++ __tests__/earthDate.test.js
@@ -4,7 +4,7 @@ describe('EarthAge', () => {
 
   test('should create an object with properties this.name, this.age, this.mercuryAge, this.venusAge, this.marsAge, and this.jupiterAge', () => {
     const myEarthAge = new EarthAge("kai", 25);
-    expect(myEarthAge).toEqual({"name": "kai", "age": 25, "mercuryAge": 104.17, "venusAge": 40.32, "marsAge": 13.30, "jupiterAge": 2.11})
+    expect(myEarthAge).toEqual({"name": "kai", "age": 25, "mercuryAge": "Mercury: 104.17", "venusAge": "Venus: 40.32", "marsAge": "Mars: 13.3", "jupiterAge": "Jupiter: 2.11"})
   });
 
   
